3a5d8c1f8d76e77ff12e8506dd57ff476e58025b
Neill Chua <Neill@172-16-14-206.DYNAPOOL.NYU.EDU>
2016-04-19 20:14:49 -0400

    auditionscontroller

--- auditionApp/auditionApp/AppDelegate.swift
+++ auditionApp/auditionApp/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let navController = window!.rootViewController as! UINavigationController
         let auditionsController = navController.topViewController as! AuditionListViewController
-        ///auditionsController.auditionStore = auditionStore
+        auditionsController.auditionStore = auditionStore
 
         
         return true
@@ -33,8 +33,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let success = auditionStore.saveChanges()
+        if (success) {
+            print("Saved all of the Auditions")
+        }
+        else {
+            print("Could not save any of the Auditions")
+        }
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
